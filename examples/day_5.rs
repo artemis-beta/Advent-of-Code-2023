6b1ee87c4a4cd852841e55f88e83ff356d7811ab
Kristian Zarębski <krizar312@gmail.com>
2023-12-06 19:24:28 +0000

    Attempted ranges

--- examples/day_5.rs
+++ examples/day_5.rs
@@ -15,7 +15,7 @@ fn main() -> () {
         None => panic!("{}", "Failed to create file path")
     };
 
-    let final_value_no_range: Vec<i64> = aoc23::day_5::parse_almanac_conversions(&file_name, false).unwrap();
+    let final_value_no_range = aoc23::day_5::parse_almanac_conversions(&file_name, false).unwrap();
 
 
     let minimum_val_no_range = match final_value_no_range.iter().min() {
@@ -23,19 +23,19 @@ fn main() -> () {
         None => panic!("Failed to retrieve minimum value")
     };
 
-    log::info!("For the almanac data given in '{}' the minimum seed location is {}", file_name, minimum_val_no_range);
+    log::info!("For the almanac data given in '{}' the minimum seed location is {}", file_name, minimum_val_no_range.0);
 
     if skip_part_2 {
         log::warn!("Skipping part 2 as inefficient..");
         return;
     }
 
-    let final_value_range: Vec<i64> = aoc23::day_5::parse_almanac_conversions(&file_name, true).unwrap();
+    let final_value_range = aoc23::day_5::parse_almanac_conversions(&file_name, true).unwrap();
 
     let minimum_val_range = match final_value_range.iter().min() {
         Some(m) => m,
         None => panic!("Failed to retrieve minimum value")
     };
 
-    log::info!("If the seed values actually specify ranges, the minimum seed location is {}", minimum_val_range);
+    log::info!("If the seed values actually specify ranges, the minimum seed location is {}", minimum_val_range.0);
 }
--- src/day_5.rs
+++ src/day_5.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
 use indexmap::IndexMap;
-use std::fs::read_to_string;
+use std::{fs::read_to_string, io};
 
 fn get_target_seeds(file_entry: &String) -> Result<Vec<i64>, String> {
     let (_, _seed_nums) = match file_entry.split_once(":") {
@@ -25,7 +25,7 @@ fn get_target_seeds(file_entry: &String) -> Result<Vec<i64>, String> {
     Ok(seeds)
 }
 
-fn get_conversions(almanac_data: &String) -> Result<IndexMap<String, Vec::<Vec<usize>>>, String> {
+fn get_conversions(almanac_data: &String) -> Result<IndexMap<String, Vec::<Vec<i32>>>, String> {
     let header_regex = match Regex::new(r"(\w+)-to-(\w+)") {
         Ok(r) => r,
         Err(e) => return Err(format!("Failed to compile header regex: {}", e))
@@ -36,9 +36,9 @@ fn get_conversions(almanac_data: &String) -> Result<IndexMap<String, Vec::<Vec<u
         Err(e) => return Err(format!("Failed to compile number regex: {}", e))
     };
 
-    let mut functions = IndexMap::<String, Vec<Vec<usize>>>::new();
-    let mut block_ranges = IndexMap::<(String, String), (usize, usize)>::new();
-    let mut lower_limit: usize = 0;
+    let mut functions = IndexMap::<String, Vec<Vec<i32>>>::new();
+    let mut block_str_index_ranges = IndexMap::<(String, String), (i32, i32)>::new();
+    let mut lower_limit: i32 = 0;
     let mut key_start: String = String::new();
     let mut key_end: String = String::new();
 
@@ -56,24 +56,24 @@ fn get_conversions(almanac_data: &String) -> Result<IndexMap<String, Vec::<Vec<u
             None => return Err(format!("Expected and category in string"))
         };
 
-        if lower_limit > 0 {block_ranges.insert((key_start.clone(), key_end.clone()), (lower_limit, start_index));}
+        if lower_limit > 0 {block_str_index_ranges.insert((key_start.clone(), key_end.clone()), (lower_limit, start_index as i32));}
 
         key_start = start.clone();
         key_end = end.clone();
-        lower_limit = end_index;
+        lower_limit = end_index as i32;
     }
 
-    block_ranges.insert((key_start.clone(), key_end.clone()), (lower_limit, almanac_data.len()));
+    block_str_index_ranges.insert((key_start.clone(), key_end.clone()), (lower_limit, almanac_data.len() as i32));
 
-    for ((start, end), (lower_lim, upper_lim)) in block_ranges {
-        let file_lines = almanac_data[lower_lim..upper_lim].lines();
+    for ((start, end), (lower_lim, upper_lim)) in block_str_index_ranges {
+        let file_lines = almanac_data[lower_lim as usize..upper_lim as usize].lines();
 
-        let mut range_definitions = Vec::<Vec<usize>>::new();
+        let mut range_definitions = Vec::<Vec<i32>>::new();
 
         for line in file_lines {
-            let mut range_components = Vec::<usize>::new();
+            let mut range_components = Vec::<i32>::new();
             for number in number_regex.find_iter(line) {
-                match number.as_str().parse::<usize>() {
+                match number.as_str().parse::<i32>() {
                     Ok(v) => range_components.push(v),
                     Err(e) => panic!("{}", e)
                 };
@@ -86,35 +86,105 @@ fn get_conversions(almanac_data: &String) -> Result<IndexMap<String, Vec::<Vec<u
     Ok(functions)
 }
 
-fn get_propagated_value(input: &i64, conversions: &IndexMap<String, Vec<Vec<usize>>>) -> Result<i64, String> {
-    log::debug!("Propagating {}...", input);
-    let mut final_val = *input;
+fn get_propagated_values(input_range: &(i32, i32), conversions: &IndexMap<String, Vec<Vec<i32>>>) -> Result<Vec<(i32, i32)>, String> {
+    log::info!("Propagating range {} <= x < {} ...", input_range.0, input_range.1);
 
     let conversion_iter = conversions.iter();
 
-    for (_, ranges) in conversion_iter {
-        for range_def in ranges {
-            let source_lower_limit = range_def[1] as i64;
-            let dest_lower_limit = range_def[0] as i64;
-            let interval = range_def[2] as i64;
-            if final_val < source_lower_limit || final_val >= source_lower_limit + interval {continue;}
-            log::debug!(
-                "Comparing {} <= {} < {} -> {} <= {} < {}",
-                source_lower_limit,
-                final_val,
-                source_lower_limit + interval,
-                dest_lower_limit,
-                (dest_lower_limit - source_lower_limit) + final_val,
-                dest_lower_limit + interval
-            );
-            final_val += dest_lower_limit - source_lower_limit;
-            break;
+    let mut pre_propagated_ranges: Vec<(i32, i32)> = vec![*input_range];
+    let mut split_ranges = Vec::<(i32, i32)>::new();
+    let mut output_ranges = Vec::<(i32, i32)>::new();
+
+    for (key, ranges) in conversion_iter {
+        log::debug!("Running mapping {}", key);
+        split_ranges.clear();
+        output_ranges.clear();
+
+        for io_range in &pre_propagated_ranges {
+            log::debug!("Checking {} <= x < {} for splits", io_range.0, io_range.1);
+            for range_def in ranges {
+                let source_lower_limit = range_def[1];
+                let interval = range_def[2];
+                let source_upper_limit = source_lower_limit + interval;
+
+                // If no overlap at all continue
+                let max_below_threshold = io_range.1 < source_lower_limit;
+                let min_above_threshold = io_range.0 >= source_upper_limit;
+                if max_below_threshold || min_above_threshold {continue;}
+
+                if io_range.0 >= source_lower_limit && io_range.1 < source_upper_limit {
+                    log::debug!("Preserved ({}, {})", io_range.0, io_range.1);
+                    split_ranges.push((io_range.0, io_range.1));
+                }
+
+                else if io_range.0 < source_lower_limit {
+                    log::debug!("Raised lower limit to: {}", source_lower_limit);
+                    split_ranges.push((source_lower_limit, io_range.1));
+                }
+
+                else if io_range.1 >= source_upper_limit {
+                    log::debug!("Lowered upper limit to: {}", source_upper_limit);
+                    split_ranges.push((io_range.0, source_upper_limit));
+                }
+            }
         }
+
+        if split_ranges.len() > 1 {log::debug!("Split ranges: {:?}", split_ranges);}
+
+        // Now propagate each of the obtained ranges
+        for split_range in &split_ranges {
+            let mut range_out: (i32, i32) = (0, 0);
+            for range_def in ranges {
+                let source_lower_limit = range_def[1];
+                let dest_lower_limit = range_def[0];
+                let interval = range_def[2];
+
+                // If no overlap at all continue
+                let max_below_threshold = split_range.1 < source_lower_limit;
+                let min_above_threshold = split_range.0 >= source_lower_limit + interval;
+                if max_below_threshold || min_above_threshold {continue;}
+
+                if split_range.0 < source_lower_limit || split_range.0 >= source_lower_limit + interval {continue;}
+
+                log::debug!(
+                    "Mapping {} <= {} < {} -> {} <= {} < {}",
+                    source_lower_limit,
+                    split_range.0,
+                    source_lower_limit + interval,
+                    dest_lower_limit,
+                    (dest_lower_limit - source_lower_limit) + split_range.0,
+                    dest_lower_limit + interval
+                );
+                log::debug!(
+                    "Mapping {} <= {} < {} -> {} <= {} < {}",
+                    source_lower_limit,
+                    split_range.1,
+                    source_lower_limit + interval,
+                    dest_lower_limit,
+                    (dest_lower_limit - source_lower_limit) + split_range.1,
+                    dest_lower_limit + interval
+                );
+                range_out.0 = (dest_lower_limit - source_lower_limit) + split_range.0;
+                range_out.1 = (dest_lower_limit - source_lower_limit) + split_range.1;
+                break;
+            }
+            output_ranges.push(range_out);
+        }
+
+        // If no output ranges, input same as output
+        if output_ranges.len() < 1 {
+            output_ranges = vec![pre_propagated_ranges[0]];
+        }
+
+        log::debug!("Mapping result: {:?}", output_ranges);
+
+        pre_propagated_ranges = output_ranges.clone();
     }
-    Ok(final_val as i64)
+
+    Ok(output_ranges)
 }
 
-pub fn parse_almanac_conversions(file_name: &String, use_ranges: bool) -> Result<Vec<i64>, String> {
+pub fn parse_almanac_conversions(file_name: &String, use_ranges: bool) -> Result<Vec<(i32, i32)>, String> {
     let file_str = match read_to_string(file_name) {
         Ok(contents) => contents,
         Err(e) => panic!("{}", e)
@@ -126,15 +196,19 @@ pub fn parse_almanac_conversions(file_name: &String, use_ranges: bool) -> Result
     };
 
     let intro_parse = get_target_seeds(&first_line.to_string())?;
-    let mut seeds = intro_parse.clone();
+    let seed_ranges: Vec<(i32, i32)>;
 
     if use_ranges {
-        seeds = intro_parse
+        seed_ranges = intro_parse
             .chunks(2)
             .filter(|x| x.len() == 2)
             .map(|x| x.to_vec())
-            .map(|x| (x[0]..x[0] + x[1]))
-            .flatten()
+            .map(|x| (x[0] as i32, (x[0] + x[1]) as i32))
+            .collect();
+    } else {
+        seed_ranges = intro_parse
+            .iter()
+            .map(|&x| (x as i32, x as i32))
             .collect();
     }
     
@@ -142,22 +216,15 @@ pub fn parse_almanac_conversions(file_name: &String, use_ranges: bool) -> Result
 
     let conversions = get_conversions(&other_content)?;
 
-    let propagated_values: Vec<i64> = seeds.iter()
-        .map(|x| get_propagated_value(x, &conversions).unwrap())
-        .collect();
+    let mut propagated_values = Vec::<(i32, i32)>::new();
 
-    if use_ranges {
-        Ok(
-            propagated_values
-            .chunks(2)
-            .filter(|x| x.len() == 2)
-            .map(|x| x.to_vec())
-            .flatten()
-            .collect()
-        )
-    } else {
-        Ok(propagated_values)
+
+    for range_set in seed_ranges {
+        let propagated_value = get_propagated_values(&range_set, &conversions)?;
+        propagated_values.extend(propagated_value);
     }
+
+    Ok(propagated_values)
 }
 
 #[cfg(test)]
@@ -167,19 +234,32 @@ mod test {
 
     #[test]
     fn test_no_match_returns_same_value() {
-        let input: i64 = 12;
-        let mut conversions = IndexMap::<String, Vec<Vec<usize>>>::new();
-        conversions.insert("test".to_string(), vec![vec![23 as usize, 45 as usize, 2 as usize]]);
-        assert_eq!(get_propagated_value(&input, &conversions).unwrap(), input);
+        let input= (12, 12);
+        let mut conversions = IndexMap::<String, Vec<Vec<i32>>>::new();
+        conversions.insert("test".to_string(), vec![vec![23 as i32, 45 as i32, 2 as i32]]);
+        let propagated_value = get_propagated_values(&input, &conversions)
+            .unwrap()
+            .iter()
+            .map(|x| x.0)
+            .min()
+            .unwrap();
+        assert_eq!(propagated_value, input.0);
     }
 
     #[test]
     fn test_single_step() {
-        let input: i64 = 12;
-        let expect: i64 = 67;
-        let mut conversions = IndexMap::<String, Vec<Vec<usize>>>::new();
-        conversions.insert("test".to_string(), vec![vec![65 as usize, 10 as usize, 6 as usize]]);
-        assert_eq!(get_propagated_value(&input, &conversions).unwrap(), expect);
+        let input = (12, 14);
+        let expect = 67;
+        let mut conversions = IndexMap::<String, Vec<Vec<i32>>>::new();
+        conversions.insert("test".to_string(), vec![vec![65 as i32, 10 as i32, 6 as i32]]);
+        let propagated_value = get_propagated_values(&input, &conversions);
+        println!("{:?}", propagated_value);
+        let temp = propagated_value.unwrap()
+            .iter()
+            .map(|x| x.0)
+            .min()
+            .unwrap();
+        assert_eq!(temp, expect);
     }
 
     #[test]
@@ -194,9 +274,9 @@ mod test {
 
         let final_value = parse_almanac_conversions(&test_file.to_str().unwrap().to_string(), false).unwrap();
 
-        let minimum_val = final_value.iter().min();
+        let minimum_val = final_value.iter().min().unwrap();
 
-        assert_eq!(*minimum_val.unwrap(), 35);
+        assert_eq!(minimum_val.0, 35);
     }
 
     #[test]
@@ -211,8 +291,8 @@ mod test {
 
         let final_value = parse_almanac_conversions(&test_file.to_str().unwrap().to_string(), true).unwrap();
 
-        let minimum_val = final_value.iter().min();
+        let minimum_val = final_value.iter().min().unwrap();
 
-        assert_eq!(*minimum_val.unwrap(), 46);
+        assert_eq!(minimum_val.0, 46);
     }
 }
